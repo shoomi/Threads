09a880c3d3167bae8d7a548387452b81de247c4b
Andriy <ashem@softserveinc.com>
2019-11-18 13:43:09 +0200

    new changes

--- src/lock/tryLock/TryLock.java
+++ src/lock/tryLock/TryLock.java
@@ -9,7 +9,8 @@ public class TryLock {
 
     public static void main(String[] args) {
         new MyTread().start();
-        new MyTread2().start();
+        MyTread2 myTread2 = new MyTread2();
+        myTread2.start();
     }
 
     static class MyTread extends Thread {
@@ -18,11 +19,11 @@ public class TryLock {
         public void run() {
             try {
                 lock.lock();
-                System.out.println(getName() + " start working");
+                System.out.println(getName() + "The main task has start working");
                 sleep(50);
-                System.out.println(getName() + " stop working");
+                System.out.println(getName() + "The main task has stop working");
                 lock.unlock();
-                System.out.println(getName() + " thread has been unlocked");
+                System.out.println(getName() + "The main thread has been unlocked");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -33,14 +34,13 @@ public class TryLock {
 
         @Override
         public void run() {
-            System.out.println(getName() + " begin working");
             while (true) {
                 // тут перевіряється чи знятий lock()
                 if (lock.tryLock()) {
-                    System.out.println(getName() + " working");
+                    System.out.println(getName() + " : I see the main task has been finished");
                     break;
                 } else {
-                    System.out.println(getName() + " waiting");
+                    System.out.println(getName() + " waiting for ending of the main task");
                 }
             }
         }
